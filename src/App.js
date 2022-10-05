a4bca39e7a8fb6a6f77798cff22413f07024369a
ijritchey <ijritchey@hotmail.com>
2022-10-04 19:32:18 -0700

    update file structure + files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import Game from './Game'
+
+
+function App() {
+    return(
+        <div>
+            <Game />
+        </div>
+    )
+}
+
+export default App;
